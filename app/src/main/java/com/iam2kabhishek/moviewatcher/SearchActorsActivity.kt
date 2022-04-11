c87662e14efbfcf60aafb73b930160251edff0aa
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-12 00:44:46 +0530

    Add search actor class

---
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/SearchActorsActivity.kt
@@ -0,0 +1,11 @@
+package com.iam2kabhishek.moviewatcher
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchActorsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_actors)
+    }
+}
