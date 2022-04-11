e62c15fa747af61c51b70468066d0f4cdf079d27
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-11 23:43:32 +0530

    Add saving classic movies functionality

--- app/src/main/java/com/iam2kabhishek/moviewatcher/MainActivity.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/MainActivity.kt
@@ -2,10 +2,23 @@ package com.iam2kabhishek.moviewatcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val movieDb = MovieDatabase.getInstance(this)
+        val movieDao = movieDb.movieDao()
+
+        val buttonAdd = findViewById<Button>(R.id.buttonAdd)
+        buttonAdd.setOnClickListener{
+            val classicMovies = MovieData.getClassicMovies()
+            for (movie in classicMovies)
+                movieDao.insert(movie)
+            Toast.makeText(this, "Classic movies added to DB", Toast.LENGTH_SHORT).show()
+        }
     }
 }
