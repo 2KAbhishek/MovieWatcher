e48bac39d584c531010fd71a7ade5f53f3fdab1a
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-11 22:56:55 +0530

    Add getInstance to MovieDatabase

--- app/src/main/java/com/iam2kabhishek/moviewatcher/MovieDatabase.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/MovieDatabase.kt
@@ -1,6 +1,26 @@
 package com.iam2kabhishek.moviewatcher
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 
-class MovieDatabase : RoomDatabase{
+@Database(entities = [Movie::class], version = 1)
+abstract class MovieDatabase : RoomDatabase() {
+
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+        private var instance: MovieDatabase? = null
+
+        @Synchronized
+        fun getInstance(ctx: Context): MovieDatabase {
+            if (instance == null)
+                instance = Room.databaseBuilder(ctx.applicationContext,
+                    MovieDatabase::class.java,
+                    "movies").fallbackToDestructiveMigration().build()
+            return instance!!
+        }
+    }
+
 }
