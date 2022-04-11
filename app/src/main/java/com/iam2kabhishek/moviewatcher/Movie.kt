f001a2881c7b43bb6a7886b2f81826a189f731bd
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-11 22:43:23 +0530

    Add movie data class

---
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/Movie.kt
@@ -0,0 +1,18 @@
+package com.iam2kabhishek.moviewatcher
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class Movie(
+    @PrimaryKey(autoGenerate = true) val title: String,
+    val year: String,
+    val rated: String,
+    val released: String,
+    val runtime: String,
+    val genre: String,
+    val director: String,
+    val writer: String,
+    val actors: String,
+    val plot: String,
+)
