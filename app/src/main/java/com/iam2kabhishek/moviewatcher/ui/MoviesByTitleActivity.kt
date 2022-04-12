5d7f755cec5eea7c44b4bb65d21f0f3bd1575613
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-12 14:45:11 +0530

    Add movies by title class

---
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/ui/MoviesByTitleActivity.kt
@@ -0,0 +1,12 @@
+package com.iam2kabhishek.moviewatcher.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.iam2kabhishek.moviewatcher.R
+
+class MoviesByTitleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movies_by_title)
+    }
+}
