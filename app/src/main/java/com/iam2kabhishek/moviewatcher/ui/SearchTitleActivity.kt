fb0146a4186f8efa964308737b590147197e63cc
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-12 14:56:55 +0530

    Rename movies by title activity

--- app/src/main/java/com/iam2kabhishek/moviewatcher/ui/SearchTitleActivity.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/ui/SearchTitleActivity.kt
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.iam2kabhishek.moviewatcher.R
 
-class MoviesByTitleActivity : AppCompatActivity() {
+class SearchTitleActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_movies_by_title)
