a079af44e91b3ee7fa423728fbfbe17d89dfb389
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-12 14:35:53 +0530

    Hide API key

--- app/src/main/java/com/iam2kabhishek/moviewatcher/data/MovieRequests.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/data/MovieRequests.kt
@@ -1,11 +1,13 @@
 package com.iam2kabhishek.moviewatcher.data
 
 import android.util.Log
+import com.iam2kabhishek.moviewatcher.BuildConfig
 import java.net.URL
 
 class MovieRequests(private val param: String) {
     fun run(): String {
-        val url = "https://www.omdbapi.com/?$param&apikey=f05230c0"
+        val apiKey = BuildConfig.API_KEY
+        val url = "https://www.omdbapi.com/?$param&apikey=$apiKey"
         val movieJson = URL(url).readText()
         Log.d("Movie response", movieJson)
         return movieJson
