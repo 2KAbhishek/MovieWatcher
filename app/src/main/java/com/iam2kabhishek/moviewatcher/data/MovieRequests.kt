10c07bf2f1c4013e64bf35f44e515152709941a5
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-12 10:49:56 +0530

    Add run method

--- app/src/main/java/com/iam2kabhishek/moviewatcher/data/MovieRequests.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/data/MovieRequests.kt
@@ -1,5 +1,12 @@
 package com.iam2kabhishek.moviewatcher.data
 
-class MovieRequests(private val url: String) {
-    fun run()
+import android.util.Log
+import java.net.URL
+
+class MovieRequests(private val param: String) {
+    fun run() {
+        val url = "https://www.omdbapi.com/?$param&apikey=f05230c0"
+        val movieJson = URL(url).readText()
+        Log.d("Movie response", movieJson)
+    }
 }
