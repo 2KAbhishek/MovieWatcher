8c7b77a1c7a9c2ca9d1dbfb0433bd750010dae1d
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-12 12:26:43 +0530

    Allow network activity on main thread

--- app/src/main/java/com/iam2kabhishek/moviewatcher/ui/SearchMovieActivity.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/ui/SearchMovieActivity.kt
@@ -2,11 +2,17 @@ package com.iam2kabhishek.moviewatcher.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.StrictMode
+import android.os.StrictMode.ThreadPolicy
 import com.iam2kabhishek.moviewatcher.R
 
 class SearchMovieActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_movie)
+
+        // Allow network activity on main thread
+        val policy = ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
     }
 }
