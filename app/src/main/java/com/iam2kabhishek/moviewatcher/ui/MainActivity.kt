a91fc226a7a30316b446472f2ce7fd396dcf8369
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-12 10:34:31 +0530

    Move ui classes into pacakge

--- app/src/main/java/com/iam2kabhishek/moviewatcher/ui/MainActivity.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/ui/MainActivity.kt
@@ -1,10 +1,11 @@
-package com.iam2kabhishek.moviewatcher
+package com.iam2kabhishek.moviewatcher.ui
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.iam2kabhishek.moviewatcher.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/iam2kabhishek/moviewatcher/ui/SearchActorsActivity.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/ui/SearchActorsActivity.kt
@@ -1,7 +1,8 @@
-package com.iam2kabhishek.moviewatcher
+package com.iam2kabhishek.moviewatcher.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.iam2kabhishek.moviewatcher.R
 
 class SearchActorsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
