9ba51d5245e5026efe787ad837fec2a17434c1cd
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-04-12 13:30:22 +0530

    Implement searching for actors in movies

--- app/src/main/java/com/iam2kabhishek/moviewatcher/ui/SearchActorsActivity.kt
+++ app/src/main/java/com/iam2kabhishek/moviewatcher/ui/SearchActorsActivity.kt
@@ -1,12 +1,35 @@
 package com.iam2kabhishek.moviewatcher.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import com.iam2kabhishek.moviewatcher.R
+import com.iam2kabhishek.moviewatcher.data.MovieDatabase
 
 class SearchActorsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search_actors)
+
+        val movieDb = MovieDatabase.getInstance(this)
+        val movieDao = movieDb.movieDao()
+
+        val editTextActor = findViewById<EditText>(R.id.editTextActor)
+        val buttonTriggerSearchActor = findViewById<Button>(R.id.buttonTriggerSearchActor)
+        val textSearchResults = findViewById<TextView>(R.id.textSearchResults)
+
+        buttonTriggerSearchActor.setOnClickListener {
+            val searchQuery = editTextActor.text.toString()
+            val movieResults = movieDao.loadActorMovies(searchQuery)
+            var searchResults = ""
+
+            for (movie in movieResults) {
+                searchResults += "$movie\n\n"
+            }
+
+            textSearchResults.text = searchResults
+        }
     }
 }
